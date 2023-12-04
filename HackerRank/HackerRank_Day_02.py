f6e99040ff877336913472894fced74ceab8c423
AakashSudan <104506051+AakashSudan@users.noreply.github.com>
2023-12-04 13:03:09 +0530

    Update HackerRank_Day_02.py

--- HackerRank/HackerRank_Day_02.py
+++ HackerRank/HackerRank_Day_02.py
@@ -66,20 +66,37 @@ if __name__ == '__main__':
 
 '''
 Problem 06
+An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+
+In the Gregorian calendar, three conditions are used to identify leap years:
+
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years. Source
+
 Task
-You are given a string .
-Your task is to find the first occurrence of an alphanumeric character in  (read from left to right) that has consecutive repetitions.
 
-Input Format
-A single line of input containing the string .
+Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
 
-Constraints
- 0 < len(S) < 100
-
-Output Format
-Print the first occurrence of the repeating character. If there are no repeating characters, print -1.
+Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
 '''
-# Code 
-import re
-s = re.search(r"([a-zA-Z0-9])(\1)", input())
-print(s.group(0)[0] if s else -1)
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year%4 == 0:
+        if year%100 == 0:
+            if year%400 == 0:
+                leap = True
+            else:
+                leap =  False
+                
+        else:
+            leap = True
+    else:
+        leap = False       
+    return leap
+
+year = int(input())
+print(is_leap(year))
